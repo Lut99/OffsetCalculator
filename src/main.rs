98d575532b3d93a2a5ce6d3aadca06147a4da28a
Lut99 <mullertim99@gmail.com>
2021-12-21 21:25:53 +0100

    Initialized project

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+/* MAIN.rs
+ *   by Lut99
+ *
+ * Created:
+ *   21 Dec 2021, 15:17:24
+ * Last edited:
+ *   21 Dec 2021, 15:20:33
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Entry point to the OffsetCalculator tool.
+**/
+
+use std::env;
+
+
+/***** ENTRY POINT *****/
+fn main() {
+    // Start by reading CLI args
+    let args: Vec<String> = env::args().collect();
+
+    // Check if there are enough
+    if args.len() > 0 {
+        // 
+    }
+
+}
